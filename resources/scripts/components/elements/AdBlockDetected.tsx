2b3bddda8696d8e37b1ccb185a129dafdb9fc35c
RKJha <sayhi@rkjha.com.np>
2022-08-30 21:07:14 +0545

    Update AdBlockDetected.tsx

--- resources/scripts/components/elements/AdBlockDetected.tsx
+++ resources/scripts/components/elements/AdBlockDetected.tsx
@@ -9,16 +9,19 @@ const AdBlockDetectedFrame = () => {
     return(
         <div className='flex flex-col text-center justify-center items-center'>
             <div>
-              <img src="https://i.imgur.com/3fPLTAL.png" alt="TeroHost" width="128" height="128"/>
+               <img src="https://i.imgur.com/3fPLTAL.png" alt="TeroHost" width="128" height="128"/>
+               <br>
             </div>
             <div>
-               <h1>ADBLOCKER DETECTED</h1>
+               <h1 className='Text-xl' >ADBLOCKER DETECTED</h1>
+               <br>
             </div>
             <div>
                It looks like you're using an ad blocker. That's okay.  Who doesn't?  
             </div>
             <div>
-                But without advertising-income, we can't keep making this site awesome.
+               But without advertising-income, we can't keep making this site awesome.
+               <br>
             </div>
             <Button  onClick={refreshPage}  className={' mt-4'}>I understand, I have disabled my ad blocker.  Let me in!</Button>
         </div>
