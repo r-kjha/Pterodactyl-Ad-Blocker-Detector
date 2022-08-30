dab274fb8a49b37f9558de29aead7b2e81b98085
RKJha <sayhi@rkjha.com.np>
2022-08-30 21:13:32 +0545

    Update AdBlockDetected.tsx

--- resources/scripts/components/elements/AdBlockDetected.tsx
+++ resources/scripts/components/elements/AdBlockDetected.tsx
@@ -14,14 +14,12 @@ const AdBlockDetectedFrame = () => {
             </div>
             <div>
                <h1 className='Text-xl' >ADBLOCKER DETECTED</h1>
-               <br>
             </div>
             <div>
                It looks like you're using an ad blocker. That's okay.  Who doesn't?  
             </div>
             <div>
                But without advertising-income, we can't keep making this site awesome.
-               <br>
             </div>
             <Button  onClick={refreshPage}  className={' mt-4'}>I understand, I have disabled my ad blocker.  Let me in!</Button>
         </div>
